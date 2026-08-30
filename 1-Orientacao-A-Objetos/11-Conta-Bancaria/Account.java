7003a795e247faf244901a3dda8d024bda4bfd48
Eduardo Henrique <eduardo.henriq1550@gmail.com>
2026-08-30 15:29:49 -0300

    Update Account.java

--- 1-Orientacao-A-Objetos/11-Conta-Bancaria/Account.java
+++ 1-Orientacao-A-Objetos/11-Conta-Bancaria/Account.java
@@ -3,53 +3,49 @@ package account.entities;
 public class Account {
 	
 	private int number; 
-	private String name; 
+	private String holder; 
 	private double balance;
 	
-	public Account() {
-		
+	public Account (int number, String holder, double initialDeposit) {
+		this.number = number; 
+		this.holder = holder; 
+		deposit(initialDeposit); 
 	}
 	
-	public Account (int number, String name, double deposit) {
+	public Account (int number, String holder) {
 		this.number = number; 
-		this.name = name; 
-		this.balance = deposit;
-	}
-	
-	public Account (int number, String name) {
-		this.number = number; 
-		this.name = name; 
+		this.holder = holder; 
 	}
 	
 	public int getNumber() {
 		return number; 
 	}
 	
-	public String getName () {
-		return name;
+	public String getHolder () {
+		return holder;
 	}
 	
-	public void setName(String name) {
-		this.name = name; 
+	public void setHolder(String holder) {
+		this.holder = holder; 
 	}
 	
 	public double getBalance() {
 		return balance;
 	}
 	
-	public void makeDeposit (double deposit) {
-		this.balance += deposit; 
+	public void deposit (double amount) {
+		this.balance += amount; 
 	}
 	
-	public void makewithDraw (double deposit) {
-		this.balance -= deposit + 5.0 ; 
+	public void withdraw (double amount) {
+		this.balance -= amount + 5.0 ; 
 	}
 	
 	public String toString() {
 		return "Account "    
 		+ number
 		+ ", Holder: "
-		+ name 
+		+ holder 
 		+ ", Balance " 
 		+ String.format("%.2f ", getBalance());
 	}
