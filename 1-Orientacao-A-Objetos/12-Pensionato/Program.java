61f5f33ad080d82f79913777cd0753474fff7d1f
Eduardo Henrique <eduardo.henriq1550@gmail.com>
2026-09-07 21:59:55 -0300

    Add files via upload

---
+++ 1-Orientacao-A-Objetos/12-Pensionato/Program.java
@@ -0,0 +1,48 @@
+package rent.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import rent.entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in); 
+		
+		int n;
+		
+		System.out.println("How many rooms will be ranted? ");
+		n = sc.nextInt();
+		
+		Rent[] vect = new Rent[9];
+		
+		System.out.println();
+		for (int i = 0; i < n; i++) {
+			sc.nextLine();
+			
+			System.out.println("Rent #" + (i+1) + ": ");
+			System.out.print("Name: ");
+			String name = sc.nextLine(); 
+			System.out.print("Email: ");
+			String email = sc.nextLine(); 
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			
+			vect[room] = new Rent(name, email);
+			System.out.println();
+		}
+		
+		System.out.println("Busy rooms: ");
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": "+ vect[i] );
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
